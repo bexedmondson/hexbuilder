ebfa0b54f7e259761c9f6753ae385847e9489c5e
Bex Edmondson <bexedmondson@gmail.com>
2025-09-19 00:28:39 +0100

    tool plus editor properties. why is godot's editor api like this. so many strings :(

--- addons/tile_placement_configurator/TileInfoGraphNode.cs
+++ addons/tile_placement_configurator/TileInfoGraphNode.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 [Tool]
@@ -10,6 +12,8 @@ public partial class TileInfoGraphNode : GraphNode
 
     public CustomTileData selectedTileData;
 
+    private Dictionary<string, EditorProperty> propertyEditors = new();
+
     private CustomTileData tileData;
     public CustomTileData customTileData
     {
@@ -20,15 +24,55 @@ public partial class TileInfoGraphNode : GraphNode
             OnCustomTileDataSet();
         }
     }
-
+    
     public void OnCustomTileDataSet()
     {
         Title = customTileData.GetFileName();
         textureRect.Texture = InjectionManager.Get<TileDatabase>().GetTileTexture(customTileData);
-        
-        
+
+        foreach (var kvp in propertyEditors)
+        {
+            kvp.Value.QueueFree();
+        }
+        propertyEditors.Clear();
+
+        var props = customTileData.GetPropertyList();
+
+        foreach (var prop in props)
+        {
+            var nameProperty = (string)prop["name"].Obj;
+            if (nameProperty == "price" || nameProperty == "baseTurnCurrencyChange")
+            {
+                var editor = EditorInspector.InstantiatePropertyEditor(customTileData, (Variant.Type)prop["type"].Obj, nameProperty, (PropertyHint)prop["hint"].Obj, (string)prop["hint_string"].Obj, Convert.ToUInt32(prop["usage"].Obj));
+                this.AddChild(editor);
+
+                editor.SetObjectAndProperty(customTileData, nameProperty);
+                editor.Label = nameProperty;
+                editor.PropertyChanged += OnPropChanged;
+                editor.Selected += OnPropSelected;
+                editor.UpdateProperty();
+                propertyEditors[nameProperty] = editor;
+            }
+        }
     }
 
+    private void OnPropChanged(StringName property, Variant value, StringName field, bool changing)
+    {
+        customTileData.Set(property, value);
+        
+        if (value.VariantType >= Variant.Type.Dictionary)
+            propertyEditors[property].UpdateProperty();
+    }
+
+    private void OnPropSelected(string path, long focusableIdx)
+    {
+        foreach (var kvp in propertyEditors)
+        {
+            if (kvp.Key != path)
+                kvp.Value.Deselect();
+        }
+    }
+    
     public void OnDeleteButton()
     {
         selectedTileData.canBePlacedOn.Remove(tileData);
