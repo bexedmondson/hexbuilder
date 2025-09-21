835d81c43385458883c1e1bf1d2b98ad49e07612
Bex Edmondson <bexedmondson@gmail.com>
2025-09-22 00:27:17 +0100

    adjacency calculation

--- scripts/GameController.cs
+++ scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class GameController : Node2D
@@ -29,8 +30,57 @@ public partial class GameController : Node2D
 
             var cellTileData = mapController.BaseMapLayer.GetCellCustomData(cell);
             totalCurrencyChange.Add(cellTileData.baseTurnCurrencyChange);
+            
+            totalCurrencyChange.Add(CalculateAdjacencyEffects(cell, cellTileData));
         }
         
         inventoryManager.OnNextTurn(totalCurrencyChange);
+    }
+
+    private CurrencySum CalculateAdjacencyEffects(Vector2I centreCell, CustomTileData centreTileData)
+    {
+        CurrencySum adjacencyEffects = new();
+
+        Dictionary<int, Dictionary<CustomTileData, AdjacencyConfig>> adjacenciesByDistance = new();
+
+        if (centreTileData.adjacencies == null)
+            return adjacencyEffects;
+        
+        
+        foreach (var adjacency in centreTileData.adjacencies)
+        {
+            if (adjacenciesByDistance.ContainsKey(adjacency.distance))
+            {
+                adjacenciesByDistance[adjacency.distance].Add(adjacency.requiredTile, adjacency);
+            }
+            else
+            {
+                adjacenciesByDistance[adjacency.distance] = new(){
+                    { adjacency.requiredTile, adjacency }
+                };
+            }
+        }
+
+        foreach (var kvp in adjacenciesByDistance)
+        {
+            var distance = kvp.Key;
+            var relevantAdjacenciesByAdjacentData = kvp.Value;
+            
+            var neighboursByDistance = mapController.GetSurroundingCellsUpToDistance(centreCell, distance);
+
+            foreach (var neighbour in neighboursByDistance.Keys)
+            {
+                var neighbourData = mapController.BaseMapLayer.GetCellCustomData(neighbour);
+                if (neighbourData == null) 
+                    continue;
+                
+                if (relevantAdjacenciesByAdjacentData.TryGetValue(neighbourData, out var relevantAdjacency))
+                {
+                    adjacencyEffects.Add(relevantAdjacency.currencyEffect);
+                }
+            }
+        }
+        
+        return adjacencyEffects;
     }
 }
