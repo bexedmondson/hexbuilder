11effc7c0645e5637657fb2df6f699b149210258
Bex Edmondson <bexedmondson@gmail.com>
2025-11-18 14:10:37 +0000

    refactoring currency display more to have single currency display in reusable scene

--- scripts/ui/CurrencyDisplay.cs
+++ scripts/ui/CurrencyDisplay.cs
@@ -6,14 +6,63 @@ public partial class CurrencyDisplay : Control
     [Export]
     private Godot.Collections.Dictionary<CurrencyType, Resource> currencyIcons;
 
+    [Export]
+    private bool showTurnChange = false;
+
+    [Export]
+    private PackedScene singleCurrencyDisplayScene;
+
+    private MapController mapController;
+
+    private Dictionary<CurrencyType, SingleCurrencyDisplay> currencyDisplays = new();
+
     public void DisplayCurrencyAmount(CurrencySum currencySum)
     {
+        CurrencySum turnChange = new();
+        if (showTurnChange)
+        {
+            mapController ??= InjectionManager.Get<MapController>();
+            foreach (var cell in mapController.BaseMapLayer.GetUsedCells())
+            {
+                if (mapController.GetCellStatus(cell) != CellStatus.Unlocked)
+                    continue;
+
+                var cellTileData = mapController.BaseMapLayer.GetCellCustomData(cell);
+                if (cellTileData?.baseTurnCurrencyChange != null)
+                    turnChange.Add(new CurrencySum(cellTileData.baseTurnCurrencyChange));
+            
+                turnChange.Add(mapController.CalculateAdjacencyEffects(cell, cellTileData));
+            }
+        }
+        
         foreach (var kvp in currencySum)
         {
             var currencyType = kvp.Key;
             var amount = kvp.Value;
             
-            var textureRect = new TextureRect();
+            var hasDisplay = currencyDisplays.TryGetValue(currencyType, out var existingSingleCurrencyDisplay);
+
+            if (!hasDisplay)
+            {
+                existingSingleCurrencyDisplay = singleCurrencyDisplayScene.Instantiate<SingleCurrencyDisplay>();
+                currencyDisplays[currencyType] = existingSingleCurrencyDisplay;
+                this.AddChild(existingSingleCurrencyDisplay);
+            }
+
+            if (showTurnChange)
+            {
+                int delta = 0;
+                turnChange.TryGetValue(kvp.Key, out delta);
+                existingSingleCurrencyDisplay.Set(currencyIcons[currencyType] as Texture2D, amount, delta);
+                existingSingleCurrencyDisplay.ShowDelta(true);
+            }
+            else
+            {
+                existingSingleCurrencyDisplay.Set(currencyIcons[currencyType] as Texture2D, amount);
+                existingSingleCurrencyDisplay.ShowDelta(false);
+            }
+            
+            /*var textureRect = new TextureRect();
             textureRect.Texture = currencyIcons[currencyType] as Texture2D;
             textureRect.CustomMinimumSize = Vector2.One * 40;
             textureRect.ExpandMode = TextureRect.ExpandModeEnum.FitHeightProportional;
@@ -23,6 +72,18 @@ public partial class CurrencyDisplay : Control
             label.LabelSettings = new LabelSettings() { FontSize = 25 };
             label.Text = amount.ToString() + "   ";
             this.AddChild(label);
+
+            if (showTurnChange)
+            {
+                var changeLabel = new Label();
+                changeLabel.LabelSettings = new LabelSettings() { FontSize = 25 };
+                
+                int delta = 0;
+                turnChange.TryGetValue(kvp.Key, out delta);
+                string prefix = delta < 0 ? "-" : "+";
+                changeLabel.Text = $"({prefix}{delta})";
+                this.AddChild(changeLabel);
+            }*/
         }
     }
 
@@ -32,5 +93,7 @@ public partial class CurrencyDisplay : Control
         {
             this.GetChild(i).QueueFree();
         }
+        
+        currencyDisplays.Clear();
     }
 }
---
+++ scripts/ui/SingleCurrencyDisplay.cs
@@ -0,0 +1,34 @@
+
+using Godot;
+
+public partial class SingleCurrencyDisplay : Control
+{
+    [Export]
+    private TextureRect icon;
+
+    [Export]
+    private Label quantityLabel;
+
+    [Export]
+    private Label changeLabel;
+
+    private readonly string plusPrefix = "+";
+    private readonly string minusPrefix = "-";
+
+    public void Set(Texture2D iconTexture, int quantity, int delta = 0)
+    {
+        icon.Texture = iconTexture;
+        quantityLabel.Text = quantity.ToString();
+        SetDelta(delta);
+    }
+
+    public void ShowDelta(bool showDelta)
+    {
+        changeLabel.Visible = showDelta;
+    }
+
+    public void SetDelta(int delta)
+    {
+        changeLabel.Text = $"({(delta < 0 ? minusPrefix : plusPrefix)}{delta})";
+    }
+}
