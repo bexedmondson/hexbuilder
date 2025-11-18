f49ab829343d2c94a0212755b674fbf16350165e
Bex Edmondson <bexedmondson@gmail.com>
2025-11-18 14:11:27 +0000

    moving adjacency calculation code

--- scripts/GameController.cs
+++ scripts/GameController.cs
@@ -37,58 +37,12 @@ public partial class GameController : Node2D
                 continue;
 
             var cellTileData = mapController.BaseMapLayer.GetCellCustomData(cell);
-            allCurrencyChanges.Add(new CurrencySum(cellTileData.baseTurnCurrencyChange));
+            if (cellTileData?.baseTurnCurrencyChange != null)
+                allCurrencyChanges.Add(new CurrencySum(cellTileData.baseTurnCurrencyChange));
             
-            allCurrencyChanges.Add(CalculateAdjacencyEffects(cell, cellTileData));
+            allCurrencyChanges.Add(mapController.CalculateAdjacencyEffects(cell, cellTileData));
         }
         
         inventoryManager.OnNextTurn(allCurrencyChanges.ToArray());
-    }
-
-    private CurrencySum CalculateAdjacencyEffects(Vector2I centreCell, CustomTileData centreTileData)
-    {
-        CurrencySum adjacencyEffects = new();
-
-        Dictionary<int, Dictionary<CustomTileData, AdjacencyConfig>> adjacenciesByDistance = new();
-
-        if (centreTileData.adjacencies == null)
-            return adjacencyEffects;
-        
-        
-        foreach (var adjacency in centreTileData.adjacencies)
-        {
-            if (adjacenciesByDistance.ContainsKey(adjacency.distance))
-            {
-                adjacenciesByDistance[adjacency.distance].Add(adjacency.requiredTile, adjacency);
-            }
-            else
-            {
-                adjacenciesByDistance[adjacency.distance] = new(){
-                    { adjacency.requiredTile, adjacency }
-                };
-            }
-        }
-
-        foreach (var kvp in adjacenciesByDistance)
-        {
-            var distance = kvp.Key;
-            var relevantAdjacenciesByAdjacentData = kvp.Value;
-            
-            var neighboursByDistance = mapController.GetSurroundingCellsUpToDistance(centreCell, distance);
-
-            foreach (var neighbour in neighboursByDistance.Keys)
-            {
-                var neighbourData = mapController.BaseMapLayer.GetCellCustomData(neighbour);
-                if (neighbourData == null) 
-                    continue;
-                
-                if (relevantAdjacenciesByAdjacentData.TryGetValue(neighbourData, out var relevantAdjacency))
-                {
-                    adjacencyEffects.Add(relevantAdjacency.currencyEffect);
-                }
-            }
-        }
-        
-        return adjacencyEffects;
     }
 }
