b3c2308947876340a27d9085384c4d893c00883d
Bex Edmondson <bexedmondson@gmail.com>
2025-12-09 17:53:14 +0000

    debug toggle shortcut

--- scripts/ui/DebugCellInfoUIContainer.cs
+++ scripts/ui/DebugCellInfoUIContainer.cs
@@ -10,10 +10,18 @@ public partial class DebugCellInfoUIContainer : Control
 
     private Dictionary<Vector2I, DebugCellInfoUI> infoUIs = new();
 
+    private bool isOn = false;
+
     public override void _Ready()
     {
         base._Ready();
         InjectionManager.Get<EventDispatcher>().Add<MapUpdatedEvent>(OnMapUpdated);
+    }
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        if (Input.IsActionJustReleased("debug_toggle"))
+            Toggle();
     }
 
     private void OnMapUpdated(MapUpdatedEvent mapUpdatedEvent)
@@ -34,9 +42,20 @@ public partial class DebugCellInfoUIContainer : Control
                 newInfoUI.Update(mapLayer.GetCellCustomData(usedCell));
                 newInfoUI.GlobalPosition = mapLayer.ToGlobal(mapLayer.MapToLocal(usedCell));
                 newInfoUI.Size = Vector2I.Zero; //for some incomprehensible reason this keeps being given a 40x40 size. resetting here.
+                newInfoUI.Visible = isOn;
                 infoUIs[usedCell] = newInfoUI;
                 this.AddChild(newInfoUI);
             }
+        }
+    }
+
+    private void Toggle()
+    {
+        isOn = !isOn;
+        
+        foreach (var infoUI in infoUIs)
+        {
+            infoUI.Value.Visible = isOn;
         }
     }
 }
