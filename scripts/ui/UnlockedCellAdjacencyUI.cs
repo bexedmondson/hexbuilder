1fde2daa61ab25f5cfd1e2ca004cffe0ec568273
Bex Edmondson <bexedmondson@gmail.com>
2025-12-19 11:46:53 +0000

    fixing coords

--- scripts/ui/UnlockedCellAdjacencyUI.cs
+++ scripts/ui/UnlockedCellAdjacencyUI.cs
@@ -22,12 +22,12 @@ public partial class UnlockedCellAdjacencyUI : Control
     private CurrencyDisplay receiveEffectDisplay;
 
     private readonly Dictionary<Vector2I, string> coordsToDirectionMap = new(){
-        { Vector2I.Down, "SW" },
-        { Vector2I.Left, "W"},
-        { Vector2I.Up, "NW"},
-        { Vector2I.Right, "E"},
-        { new Vector2I(1, -1), "NE"},
-        { new Vector2I(-1, 1), "SE"}
+        { Vector2I.Down, "NW" },
+        { Vector2I.Left, "NE"},
+        { Vector2I.Up, "SE"},
+        { Vector2I.Right, "SW"},
+        { new Vector2I(1, -1), "S"},
+        { new Vector2I(-1, 1), "N"}
     };
 
     public void Setup(CustomTileData adjacentTileData, Vector2I centreCell, Vector2I adjacentCell)
