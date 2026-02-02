2cbc5ee39111f9ff51bb026d310c2ce45bda66ef
Bex Edmondson <bexedmondson@gmail.com>
2026-02-02 20:36:39 +0000

    updating blender batch rendering script

--- blender/glb_render_all.py
+++ blender/glb_render_all.py
@@ -4,19 +4,28 @@ dirpath = '/home/bex/Documents/blender/hexagonset_renders'
 
 def set_render_self_and_children(glb, show):
     glb.hide_render = not show
+    #print("  setting " + str(glb) + " to " + str(show))
+    #print("  children: " + str(glb.children))
     for child in glb.children:
         child.hide_render = not show
+        #print("  setting " + str(child) + " to " + str(show))
 
-glbs = list(bpy.data.collections["GLBs"].all_objects)
+glbs = list(bpy.data.collections["GLBs"].objects)
 
 object = None
 
 for glb in glbs:
+    if glb.parent != None:
+        continue
+    #print("rendering " + str(glb))
     set_render_self_and_children(glb, True)
     
     for other in glbs:
         if glb == other:
             continue
+        if other.parent != None:
+            continue
+        #print("checking " + str(other))
         set_render_self_and_children(other, False)
     
     bpy.context.scene.render.filepath = os.path.join(dirpath, glb.name + '.png')
